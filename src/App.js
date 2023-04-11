317a02e994f8f5a70c2bcca05cd7bae32f2c359a
hakareem <harithakareem72@gmail.com>
2023-04-11 14:33:11 +0100

    backend call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import "./App.css";
 
 function App() {
+  const data = {
+    username: "mike",
+    password: "asdfasdf",
+  };
+
+  fetch("api/auth/login", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: new Headers({
+      "Content-Type": "application/json; charset=UTF-8",
+    }),
+  })
+    .then((res) => {
+      return Promise.all([res.json(), ...res.headers]);
+    })
+    .then((data, headers) => {
+      console.log(headers);
+      console.log(data);
+    });
+
   return <div className='App'></div>;
 }
 
