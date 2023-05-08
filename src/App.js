18010d7ac8d854a2fb733af25b92055f5961c1cc
Harith A Kareem <93913130+hakareem@users.noreply.github.com>
2023-05-08 12:21:58 +0100

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
+import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  const data = {
-    username: "mike",
-    password: "asdfasdf",
-  };
 
-  fetch("api/auth/login", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: new Headers({
-      "Content-Type": "application/json; charset=UTF-8",
-    }),
-  })
-    .then((res) => {
-      return Promise.all([res.json(), res.headers]);
-    })
-    .then(([data, headers]) => {
-      const authorisationValue = headers.get("authorization")
-      console.log(authorisationValue)
-    });
+  const [jwt, setJwt] = useState("")
 
-  return <div className='App'></div>;
+  useEffect(() => {
+      const data = {
+        username: "mike",
+        password: "asdfasdf",
+      };
+
+    fetch("api/auth/login", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: new Headers({
+          "Content-Type": "application/json; charset=UTF-8",
+        }),
+      })
+        .then((res) => {
+          return Promise.all([res.json(), res.headers]);
+        })
+        .then(([data, headers]) => {
+          setJwt(headers.get("authorization"))
+          console.log(jwt)
+        });
+  }, [jwt])
+
+  return (
+    <div className='App'>
+      <p>JWT VALUE IS: {jwt}</p>
+    </div>
+     )
 }
 
 export default App;
