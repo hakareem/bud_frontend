1504284aa56b9a3060c56863f5fd62b3c85b6f85
Harith A Kareem <93913130+hakareem@users.noreply.github.com>
2023-05-15 10:21:12 +0100

    CRUD operation

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -3,9 +3,25 @@ import useLocalStorage from '../util/useLocalStorage';
 
 const Dashboard = () => {
     const [jwt, setJwt] = useLocalStorage("", "jwt")
+
+    function createAssignment() {
+        fetch("api/assignments", {
+            method: "POST",
+            headers: new Headers({
+            "Content-Type": "application/json; charset=UTF-8",
+            "Authorization": `Bearer ${jwt}`
+            }),
+        }).then((res) => {
+            if(res.status === 200) {
+                return res.json()
+            }
+        }).then((assigment) => {
+            window.location.href = `/assignments/${assigment.id}`
+        })
+    }
         return (
-            <div>
-                <p>JWT VALUE IS {jwt}</p>
+            <div style={{margin: "2em"}}>
+                <button id='submit' type='submit' onClick={() => createAssignment()}>Add New Assigment</button>
             </div>
         );
 };
