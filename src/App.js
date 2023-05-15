8c90379fffd835e13b5c3891c584c43f8c5c48d8
Harith A Kareem <93913130+hakareem@users.noreply.github.com>
2023-05-15 10:20:18 +0100

    old fetch

--- src/App.js
+++ src/App.js
@@ -9,32 +9,6 @@ import PrivateRoute from "./PrivateRoute/PrivateRoute";
 
 function App() {
 
-  const [jwt, setJwt] = useLocalStorage("", "jwt")
-
-  // useEffect(() => {
-  //   if (!jwt) {
-  //     const data = {
-  //       username: "mike",
-  //       password: "asdfasdf",
-  //     };
-      
-  //     fetch("api/auth/login", {
-  //       method: "POST",
-  //       body: JSON.stringify(data),
-  //       headers: new Headers({
-  //         "Content-Type": "application/json; charset=UTF-8",
-  //       }),
-  //     })
-  //     .then((res) => {
-  //       return Promise.all([res.json(), res.headers]);
-  //     })
-  //     .then(([data, headers]) => {
-  //       setJwt(headers.get("authorization"))
-  //       console.log(jwt)
-  //     });
-  //   }
-  //   }, [jwt])
-
   return (
     <Routes>
       <Route path="dashboard" element={
