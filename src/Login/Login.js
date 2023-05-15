98b7184f68e3225f76e73eb3a42ade2a61219238
Harith A Kareem <93913130+hakareem@users.noreply.github.com>
2023-05-15 10:20:51 +0100

    login validation

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useLocalStorage from '../util/useLocalStorage';
 
 const Login = () => {
+    const [jwt, setJwt] = useLocalStorage("", "jwt")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+      const data = {
+        username: username,
+        password: password,
+      };
+      
+      fetch("api/auth/login", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: new Headers({
+          "Content-Type": "application/json; charset=UTF-8",
+        }),
+      })
+      .then((res) => {
+        if(res.status == 200) 
+            return Promise.all([res.json(), res.headers]);
+        else 
+            return Promise.reject("Invalid login attempt")
+      })
+      .then(([data, headers]) => {
+        setJwt(headers.get("authorization"))
+        window.location.href = "dashboard"
+      }).catch((err) => {
+        console.log(err)
+      })
+
+    function sendLoginRequest() {
+        console.log("HEY TESTING REQUEST")
+    }
+
     return (
         <>
             <div>
                 <label htmlFor='username'>Username</label>
-                <input type="email" id="username" />
+                <input type="email" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
             </div>
             <div>
                 <label htmlFor='password'>Password</label>
-                <input type="password" id="password" />
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div>
+                <button id='submit' type='button' onClick={() => sendLoginRequest()}>Send</button>
             </div>
         </>
     );
