9e63543c8bd4fa7b5dc2f8c5302ad015ed803ca3
Harith A Kareem <harithakareem72@gmail.com>
2025-03-14 11:41:43 +0000

    dashboard error handling

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -2,28 +2,32 @@ import React from 'react';
 import useLocalStorage from '../util/useLocalStorage';
 
 const Dashboard = () => {
-    const [jwt, setJwt] = useLocalStorage("", "jwt")
+    const [jwt] = useLocalStorage("", "jwt");
 
-    function createAssignment() {
-        fetch("api/assignments", {
-            method: "POST",
-            headers: new Headers({
-            "Content-Type": "application/json; charset=UTF-8",
-            "Authorization": `Bearer ${jwt}`
-            }),
-        }).then((res) => {
-            if(res.status === 200) {
-                return res.json()
+    const createAssignment = async () => {
+        try {
+            const response = await fetch("api/assignments", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json; charset=UTF-8",
+                    "Authorization": `Bearer ${jwt}`
+                },
+            });
+
+            if (response.ok) {
+                const assignment = await response.json();
+                window.location.href = `/assignments/${assignment.id}`;
             }
-        }).then((assigment) => {
-            window.location.href = `/assignments/${assigment.id}`
-        })
-    }
-        return (
-            <div style={{margin: "2em"}}>
-                <button id='submit' type='submit' onClick={() => createAssignment()}>Add New Assigment</button>
-            </div>
-        );
+        } catch (error) {
+            console.error("Error creating assignment:", error);
+        }
+    };
+
+    return (
+        <div style={{ margin: "2em" }}>
+            <button id='submit' type='submit' onClick={createAssignment}>Add New Assignment</button>
+        </div>
+    );
 };
 
 export default Dashboard;
