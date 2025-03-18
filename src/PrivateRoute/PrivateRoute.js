d4af0740027c74aadab1d1298f172442b73c7edf
Harith A Kareem <harithakareem72@gmail.com>
2025-03-18 08:56:50 +0000

    jwt validation

--- src/PrivateRoute/PrivateRoute.js
+++ src/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useLocalStorage from '../util/useLocalStorage';
 import { Navigate } from 'react-router-dom';
+import ajax from '../Services/fetchService';
 
-const PrivateRoute = ({children}) => {
-    const [jwt, setJwt] = useLocalStorage("", "jwt")
+const PrivateRoute = ({ children }) => {
+    const [jwt] = useLocalStorage("", "jwt");
+    const [isValid, setIsValid] = useState(null);
 
-    return jwt ? children : <Navigate to="/login" />
+    if(!jwt) {
+        return <Navigate to="/login" />;
+    } 
+
+    useEffect(() => {
+        const validateToken = async () => {
+            try {
+                const response = await ajax("api/auth/validate", jwt, "GET");
+                const isValid = await response.json();
+                setIsValid(isValid);
+            } catch (error) {
+                console.error("Error validating JWT", error);
+                setIsValid(false);
+            }
+        };
+
+        validateToken();
+    }, []);
+
+    if (isValid === null) {
+        return <p>Loading...</p>;
+    }
+
+    return isValid ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
